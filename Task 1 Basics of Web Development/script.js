abd966d1ffa3a1f61a77ec976a6a32d2d65988e5
Mythu25 <nnsheela43@gmail.com>
2025-06-19 12:30:21 +0530

    Create script.js

---
+++ Task 1 Basics of Web Development/script.js
@@ -0,0 +1,9 @@
+<script>
+   document.getElementById("buyBtn").addEventListener("click",function() { 
+    alert("Thank you for choosing StyleEra! Your order is being processed.")
+    });
+   document.getElementById("cartBtn").addEventListener("click",function()
+   { 
+    alert("Item added to your cart successfully!")
+   });
+</script>
